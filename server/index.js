d92e86f6ee7e9647c385d0dd3d41c66310fad565
Neil Dudani <neil.dudani@gmail.com>
2021-06-23 23:51:58 -0400

    created scripts to load data into photos and characteristics

--- server/index.js
+++ server/index.js
@@ -14,26 +14,22 @@ const client = new Client({
 
 client.connect();
 
-// Photos loading function into Database Using Chunks
+// ******************************* PHOTOS *********************** \\
 const photos = './../csv_data/reviews_photos.csv';
 const photosPath = path.join(__dirname, photos);
 const photosReadStream = fs.createReadStream(photosPath, {encoding: 'utf8'})
-const photosLoaded = false;
-
+let photosLoaded = false;
 let photosLineRemainder = '';
 
 function loadPhotos() {
 
   photosReadStream.on('data', async (chunk) => {
-
     photosReadStream.pause();
-
     let lines = chunk.split('\n');
     lines[0] = photosLineRemainder + lines[0];
     photosLineRemainder = lines.pop();
     let firstLine = lines[0];
     let i = 0;
-
     while (i < lines.length) {
       let splitArray = lines[i].split(',');
       let photo_id = splitArray[0];
@@ -50,23 +46,52 @@ function loadPhotos() {
 
   photosReadStream.on('end', () => {
     console.log('Finished photos!');
-    photosLoaded = true;
+    loadCharacteristics();
   });
 
 };
 
+// ******************************* CHARACTERISTICS *********************** \\
+const characteristics = './../csv_data/characteristics.csv';
+const characteristicsPath = path.join(__dirname, characteristics);
+const characteristicsReadStream = fs.createReadStream(characteristicsPath, {encoding: 'utf8'})
+let characteristicsLoaded = false;
+let characteristicsLineRemainder = '';
 
-//Invoke createReadStream events
+function loadCharacteristics() {
+
+  characteristicsReadStream.on('data', async (chunk) => {
+    characteristicsReadStream.pause();
+    let lines = chunk.split('\n');
+    lines[0] = characteristicsLineRemainder + lines[0];
+    characteristicssLineRemainder = lines.pop();
+    let firstLine = lines[0];
+    let i = 0;
+    while (i < lines.length) {
+      let splitArray = lines[i].split(',');
+      let characteristic_id = splitArray[0];
+      let product_id = splitArray[1];
+      let characteristic = splitArray[2];
+      let sql = `INSERT INTO characteristics (characteristic_id, product_id, characteristic) VALUES (${characteristic_id}, ${product_id}, '${characteristic}')`;
+      await client.query(sql);
+      i++;
+      if (i === lines.length - 1) {
+        characteristicsReadStream.resume();
+      }
+    }
+  });
+
+  characteristicsReadStream.on('end', () => {
+    console.log('Finished characteristics!');
+  });
+
+};
+// ******************************* META *********************** \\
+
+// ******************************* REVIEWS *********************** \\
+
+//Begin chain of createReadStream events
 loadPhotos();
-if (photosLoaded) {loadCharacteristics()};
-
-
-
-
-
-
-
-
 
 // Handle Requests
 app.get('/', (req, res) => {
