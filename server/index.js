9be25709a46eab409fade43d10d6e2d1d0aa75c0
Neil Dudani <neil.dudani@gmail.com>
2021-06-24 20:43:35 -0400

    Created routes with test message sent back to client

--- server/index.js
+++ server/index.js
@@ -15,9 +15,30 @@ const client = new Client({
 client.connect();
 
 // Handle Requests
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get('/reviews', (req, res) => {
+  res.send('detailed reviews!')
+  let productId = req.query.product_id;
+});
+
+app.get('/reviews/meta', (req, res) => {
+  res.send('meta')
+  let productId = req.query.product_id;
+});
+
+app.put('/reviews/:review_id/helpful', (req, res) => {
+  let productId = req.params.review_id;
+  res.send('helpful');
+});
+
+app.put('/reviews/:review_id/report', (req, res) => {
+  let productId = req.params.review_id;
+  res.send('report');
+});
+
+app.post('/reviews', (req, res) => {
+  res.send('post');
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
