936626414db3cf24cd8cc2b009f63343415b1f00
Neil Dudani <neil.dudani@gmail.com>
2021-06-28 19:02:34 -0400

    removed retry logic as not needed

--- server/index.js
+++ server/index.js
@@ -3,21 +3,6 @@ const port = 5000;
 
 app.listen(port, () => {
   console.log(`Actual server has started on port ${port}!`);
-  ConnectToDb();
 });
 
-async function ConnectToDb() {
-  let retries = 5;
-  while (retries) {
-    try {
-      await client.connect();
-      break;
-    } catch (err) {
-      console.log('error: ', err);
-      retries -= 1;
-      console.log(`retries left: ${retries}`);
-      await new Promise(res => setTimeout(res, 5000));
-    }
-  }
-}
-
+client.connect();
