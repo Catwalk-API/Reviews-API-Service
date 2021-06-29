8051f96852615c74682f7c175ddda640b7791a1a
Neil Dudani <neil.dudani@gmail.com>
2021-06-29 16:42:46 -0400

    refactored tests to be more efficient

--- server/queries.js
+++ server/queries.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   sqlA: function(productId, count, offset, client) {
-    let sql = `SELECT review_id, rating, summary, recommend, response, body, date, reviewer_name, helpfulnessCount FROM reviews WHERE product_id = ${productId} AND reported = 'false' LIMIT ${count} OFFSET ${offset}`;
+    let sql = `SELECT review_id, rating, summary, recommend, response, body, date, reviewer_name, helpfulnessCount FROM reviews WHERE product_id = ${productId} AND reported is not true LIMIT ${count} OFFSET ${offset}`;
     return client.query(sql);
   }
 }
